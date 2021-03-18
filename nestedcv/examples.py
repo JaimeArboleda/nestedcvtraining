e595bb947d27329fcb65da9e775d1dd7c4497946
JaimeArboleda <jaime.arboleda.castilla@gmail.com>
2021-03-18 07:37:29 +0100

    Examples updated with public github dataset

--- nestedcv/examples.py
+++ nestedcv/examples.py
@@ -14,11 +14,11 @@ from train import find_best_binary_model
 dict_pipelines_post_process = {
         'option_1': Pipeline([
             ('scale', StandardScaler()),
-            ('reduce_dims', PCA(n_components=50))
+            ('reduce_dims', PCA(n_components=5))
         ]),
         'option_2': Pipeline([
             ('scale', StandardScaler()),
-            ('reduce_dims', SelectKBest(mutual_info_classif, k=100))
+            ('reduce_dims', SelectKBest(mutual_info_classif, k=5))
         ]),
         'option_3': Pipeline([
             ('identity', MidasIdentity())
@@ -88,12 +88,10 @@ dict_models_example = {
     }
 }
 if __name__ == "__main__":
-    dataset = pd.read_csv('D:/Python/nestedcvtraining/nestedcv/dataset_completo.csv', **{'sep': ";", 'index_col': False, 'decimal': ','})
-    index_cols = ['NIF Identificado', 'Ejercicio']
-    target_col = ['target']
-    dataset = dataset.sample(n=1000)
-    y = dataset['target'].to_numpy()
-    X = dataset[[c for c in dataset.columns if c not in index_cols + target_col]].to_numpy()
+    dataset_prueba = pd.read_csv(
+        'https://raw.githubusercontent.com/shraddha-an/cleansed-datasets/master/credit_approval.csv')
+    y = dataset_prueba['Target'].to_numpy()
+    X = dataset_prueba[[c for c in dataset_prueba.columns if c != 'Target']].to_numpy()
     dict_models = {
         'xgboost': {
             'model': XGBClassifier(),
@@ -114,26 +112,28 @@ if __name__ == "__main__":
                 Categorical(['binary:logistic'], name='model__objective')
             ]
         },
-        'lightgbm': {
-            'model': lgb.LGBMClassifier(),
-            'pipeline_post_process': Pipeline([("scale", StandardScaler())]),
+        'random_forest': {
+            'model': RandomForestClassifier(),
+            'pipeline_post_process': Pipeline([
+                ('scale', StandardScaler())
+            ]),
             'search_space': [
                 Categorical([True, False], name='undersampling_majority_class'),
-                Real(0.01, 0.5, prior='log-uniform', name='model__learning_rate'),
-                Integer(1, 30, name='model__max_depth'),
-                Integer(10, 400, name='model__num_leaves'),
-                Real(0.1, 1.0, prior='uniform', name='model__feature_fraction'),
-                Real(0.1, 1.0, prior='uniform', name='model__subsample'),
-                Categorical(['balanced'], name='model__class_weight'),
-                Categorical(['binary'], name='model__objective')
+                Integer(0, 1, name='model__bootstrap'),
+                Integer(10, 100, name='model__n_estimators'),
+                Integer(2, 10, name='model__max_depth'),
+                Integer(5, 20, name='model__min_samples_split'),
+                Integer(1, 4, name='model__min_samples_leaf'),
+                Categorical(['auto', 'sqrt'], name='model__max_features'),
+                Categorical(['balanced', 'balanced_subsample'], name='model__class_weight')
             ]
-        }
+        },
     }
 
 
     best_model, document = find_best_binary_model(X=X, y=y, model_search_spaces=dict_models,
-                                                   verbose=True, k_inner_fold=5, k_outer_fold=5,
-                                                   skip_inner_folds=[0, 2], skip_outer_folds=[0, 2, 4],
+                                                   verbose=True, k_inner_fold=10, k_outer_fold=10,
+                                                   skip_inner_folds=[0, 2, 4, 6, 8], skip_outer_folds=[0, 2, 4, 6, 8],
                                                    n_initial_points=10, n_calls=10, loss_metric='average_precision',
                                                    peeking_metrics=['roc_auc', 'neg_log_loss', 'average_precision', 'neg_brier_score'])
-    document.save('report_013v2.docx')
+    document.save('report_dataset_prueba.docx')
